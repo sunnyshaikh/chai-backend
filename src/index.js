624f96544b6e7b7d593dde40f9d3953af1bec39f
AltafAlam Shaikh <gitforaltaf@gmail.com>
2024-03-20 23:52:51 +0530

    configuration/middleware setup and custom error, response handler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config();
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server started on PORT " + process.env.PORT || 8000);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection error !!", err);
+  });
